1387db942dbb60caf884e6c72a8579441ee31124
lss <lss@email.com>
2024-05-17 14:15:27 +0900

    refactor assert case function

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -5,18 +5,26 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNum) {
+		assertIllegalArgument(guessNum);
+	}
+	void assertIllegalArgument(const std::string& guessNum)
+	{
 		if (guessNum.length() != 3)
 			throw length_error("Must be three letters.");
 
 		for (char ch : guessNum) {
-			if (ch < '0' || ch > '9')
-				throw invalid_argument("Must bo number.");
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must bo number.");
 		}
 
-		if (guessNum[0] == guessNum[1]
-			|| guessNum[0] == guessNum[2]
-			|| guessNum[1] == guessNum[2]) {
+		if (isDuplicateNumber(guessNum)) {
 			throw invalid_argument("Must not have the same number.");
 		}
+	}
+	bool isDuplicateNumber(const std::string& guessNum)
+	{
+		return guessNum[0] == guessNum[1]
+			|| guessNum[0] == guessNum[2]
+			|| guessNum[1] == guessNum[2];
 	}
 };
